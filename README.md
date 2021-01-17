7669cf1e6d2460b6fedf5a51f8b9e024da0900cf
Pythonidaer <pythonidaer@gmail.com>
2021-01-16 22:51:57 -0500

    set up basic html/css/js connections and performed light pseudo code for tomorrow

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # DayPlanner
+
+# DayPlanner
+*Daily Scheduler utilizing JavaScript and localStorage*
+
+[View Deployed Application Here.](https://pythonidaer.github.io/DayPlanner/)
+
+## Installation
+1. Clone repository to your local device
+2. Test by opening the index.html file in your browser. If it looks good, it's working right.
+3. If it does not work, right-click Inspect console to determine if there is an issue with the directories.
+
+## Usage
+This repo is simply a homework assignment. Feel free to inspect the code and notice how I made a Daily Scheduler application:
+
+  
+## Credits
+[Stack Overflow](https://stackoverflow.com/questions/16562577/how-can-i-make-a-button-redirect-my-page-to-another-page) 
+
+
+## Future Improvements
+
+  
+## License
+I've chosen an MIT License. Do what you'd like with this material.
+
+#### The Website
+![Daily Scheduler]()
---
+++ assets/scripts/behavior.js
@@ -0,0 +1,39 @@
+console.log('test')
+
+/*
+What I want to do is watch the .gif
+I want to watch the .gif to see how one block is made.
+I want to build one of the blocks with HTML/CSS.
+Once I build the block I want to create an array of 9-5 hours.
+Once I build the array, I want to loop over a function.
+I want this function to create a block for each (similar to CodingQuiz code).
+Once the form is there, local storage and editable input should be the challenge.
+*/
+
+// display timeblocks for standard business hours (9am 5pm)
+
+// each time block contains an input field & a save button
+
+// time block save button saves the input text to localStorage, allowing the text to persist when the app is refreshed.
+
+// current day displayed at the top of the calendar (Moment.js / Day.js etc)
+
+// each time block color coded to indicate whether it is in the past, present or future (hour) -- looks like this will compare 9am to the actual time
+
+// Uses moment.js to work with data & time or Luxox, Day.js, date-fns or js-Joda
+
+// AS AN employee who's busy, I WANT to add events to a planner SO THAT I can manage time effectively
+
+// GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+// WHEN I scroll down
+// THEN I'm presented with time blocks for standard business hours
+// WHEN I view the blocks for that day
+// THEN each time block color-coded: past, present or future
+// WHEN I click into a time block
+// THEN I can enter an event
+// WHEN I click the save button for that time block
+// THEN the text for that event is saved in localStorage
+// WHEN I refreshed the page
+// THEN the saved events persist
